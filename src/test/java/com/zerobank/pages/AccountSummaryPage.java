fe909bca72a3e0e865f3b338debf59e4540c530d
eywas <umiteywas@gmail.com>
2023-08-02 22:45:34 +0300

    02 agust 2023

--- src/test/java/com/zerobank/pages/AccountSummaryPage.java
+++ src/test/java/com/zerobank/pages/AccountSummaryPage.java
@@ -1,2 +1,22 @@
-package com.zerobank.pages;public class AccountSummaryPage {
+package com.zerobank.pages;
+
+import com.zerobank.utilities.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class AccountSummaryPage extends BasePage{
+
+    @FindBy(css = ".board-header")
+    private List<WebElement> accountTypes;
+    @FindBy(xpath = "(//table[@class='table'])[3]//th ")
+    private List<WebElement> columns;
+
+    public List<String> getAccountTypesText(){
+        return BrowserUtils.getElementsText(accountTypes);
+    }
+    public  List<String> getColumnNames(){
+        return BrowserUtils.getElementsText(columns);
+    }
 }
